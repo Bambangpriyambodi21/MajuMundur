2c34bcccaa7d2970e93a30fff06c60feaec4f0d7
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-05-11 20:37:10 +0700

    [IMPROVE] fix banget

--- src/main/java/com/enigma/tokopakedi/repository/CustomerRepository.java
+++ src/main/java/com/enigma/tokopakedi/repository/CustomerRepository.java
@@ -2,12 +2,17 @@ package com.enigma.tokopakedi.repository;
 
 import com.enigma.tokopakedi.entity.Customer;
 import com.enigma.tokopakedi.entity.Product;
+import com.enigma.tokopakedi.entity.UserCredential;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, String>, JpaSpecificationExecutor<Customer> {
